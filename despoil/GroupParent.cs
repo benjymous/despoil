3b222f5ed80295524d888fe771f349bc0aaec2bb
benjymous <r.munn@outlook.com>
2022-08-09 16:08:20 +0100

    Move collections into groups to match threads

--- despoil/GroupParent.cs
+++ despoil/GroupParent.cs
@@ -1,6 +1,6 @@
 ﻿namespace despoil
 {
-    public class ThreadGroup
+    public class GroupParent
     {
         public string name { get; set; } = string.Empty;
         public IssueGroup[]? Groups { get; set; } = null;
